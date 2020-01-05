972ca3a756b74af0aa942a9aa85d4465e184db92
suphakit108 <47696371+suphakit108@users.noreply.github.com>
2020-01-05 23:28:20 +0700

    11

--- choo/Moter/Moter.ino
+++ choo/Moter/Moter.ino
@@ -14,23 +14,30 @@
 const int analogIn=A0;
 int RawValue;
 
-#define MCP_LEDTOG1 15
+#define MCP_LEDTOG1 1
+//mcp                      
+#define MCP_LEDTOG2 0
 
 Adafruit_MCP23017 mcp;
   
 void setup() {  
   mcp.begin();      // Default device address 0
 Serial.begin (9600);
-  mcp.pinMode(15, OUTPUT);  // Toggle LED 1
+  mcp.pinMode(0, OUTPUT);  // Toggle LED 1
+    mcp.pinMode(1, OUTPUT);  // Toggle LED 1
 
 }
 
 void loop() {
    Serial.println("Start");
-  mcp.digitalWrite(15,HIGH);
+   
+  mcp.digitalWrite(MCP_LEDTOG2, 0);
+  mcp.digitalWrite(MCP_LEDTOG1, 1023);
+  
   delay(1000);
 
-  mcp.digitalWrite(15,LOW);
+  mcp.digitalWrite(MCP_LEDTOG2, 0);
+  mcp.digitalWrite(MCP_LEDTOG1, 0);
  delay(1000);
  Serial.println("DoNE");
 }
